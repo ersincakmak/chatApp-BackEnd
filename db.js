d88c04d34a9501110d1e16dec9e58e73b82c49b8
ersin cakmak <ckmrsn@gmail.com>
2021-05-31 22:17:55 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const firebase = require("firebase")
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: "AIzaSyCEWEXNA1u_plBMsSd7BjRPEHFd9yxotbw",
+  authDomain: "iot-p1-4c39d.firebaseapp.com",
+  databaseURL: "https://iot-p1-4c39d.firebaseio.com",
+  projectId: "iot-p1-4c39d",
+  storageBucket: "iot-p1-4c39d.appspot.com",
+  messagingSenderId: "502896039378",
+  appId: "1:502896039378:web:87338535544eec66b7240b",
 }
 
 const db = firebase.initializeApp(firebaseConfig)
